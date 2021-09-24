aedd403f98a5abd90526a5d1bf307062c369de53
Vitalii <puzanov.vitaly@gmail.com>
2021-09-24 09:37:28 +0300

    Refactoring and add some function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
-import './App.css';
-import { useState } from 'react';
-import Main from './Main';
-import Sidebar from './Sidebar';
-import uuid from 'react'
+import {useState, useEffect} from 'react';
+
+import Main from './components/Main/Main';
+import Sidebar from './components/Sidebar/Sidebar';
+
+import './Style.css';
 
 function App() {
+  function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c === 'x' ? r : r && 0x3 | 0x8;
+        return v.toString(16);
+      },
+    );
+  }
 
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(JSON.parse(localStorage.notes) || []);
   const [activeNote, setActiveNote] = useState(false);
-  
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
   const onAddNote = () => {
-    const newNote={
-      id: uuid(),
-      title: "Untitled Node",
-      body: "",
+    const newNote = {
+      id: uuidv4(),
+      title: 'Untitled Node',
+      body: '',
       lastModified: Date.now(),
-    }
+    };
     setNotes([newNote, ...notes]);
   };
 
-
-  const onDeleteNote = (id) =>{
+  const onDeleteNote = (id) => {
     setNotes(notes.filter((note) => note.id !== id));
   };
 
-   
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
 
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
 
-  
+      return note;
+    });
+
+    setNotes(updatedNotesArr);
+  };
+
   return (
     <div className="App">
       <Sidebar
@@ -37,7 +63,7 @@ function App() {
         activeNote={activeNote}
         setActiveNote={setActiveNote}
       />
-      <Main/>
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Main({activeNote, onUpdateNote}) {
+  const onEditField = (key, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [key]: value,
+      lastModified: Date.now(),
+    });
+  };
+
+  if (!activeNote) return <div className="no-active-note">No notes</div>;
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          id="title"
+          placeholder="Note Title"
+          value={activeNote.title}
+          onChange={(e) => onEditField('title', e.target.value)}
+          autoFocus></input>
+        <textarea
+          id="body"
+          placeholder="Write your note here..."
+          value={activeNote.body}
+          onChange={(e) => onEditField('body', e.target.value)}
+        />
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <div className="markdown-preview">{activeNote.body}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
